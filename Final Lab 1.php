65d725eaca06542548ef5988fce97829e90b2fc5
lamyaAlif133 <lamyaalif133@gmail.com>
2025-04-28 14:33:47 +0600

    Final

---
+++ Final Lab 1.php
@@ -0,0 +1,8 @@
+<?php
+$length=20;
+$width=10;
+$Area=$length*$width;
+$Perimeter= 2*$length*$width;
+echo "The Area of rectangle: {$Area} <br>";
+echo "The Perimeter of rectangle: {$Perimeter} <br>";
+?>
